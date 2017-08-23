f975ef99de2fd85016685901658eae8710b8dd2c
kissa1001 <kissa101295@gmail.com>
2017-08-23 15:22:52 -0700

    updated mongoDB

--- client/components/link_create.jsx
+++ client/components/link_create.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: '' }
+  }
   handleSubmit(e) {
     e.preventDefault();
-    Meteor.call('links.insert', this.refs.link.value);
+    Meteor.call('links.insert', this.refs.link.value, (error) => {
+      if(error) {
+        this.setState({error: 'Enter a valid URL'});
+      } else {
+        this.setState({error: ''});
+        this.refs.link.value = '';
+      }
+    });
   }
   render() {
     return(
@@ -13,6 +24,7 @@ class LinkCreate extends Component {
             <label>Link to shorten!</label>
             <input ref="link" className="form-control" placeholder="http://example.com/put-your-long-link-here"/>
           </div>
+          <div className="text-danger">{this.state.error}</div>
           <button className="btn btn-primary">Shorten!</button>
         </form>
       </div>
